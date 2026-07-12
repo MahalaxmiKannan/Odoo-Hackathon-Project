501e1f0a5d4ed5ec45384e71582f384dda69638c
MahalaxmiKannan <mahalaxmikannan51@gmail.com>
2026-07-12 14:36:16 +0530

    Created category files

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,8 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/departments", require("./routes/department.routes"));
+app.use("/api/categories",require("./routes/category.routes"));
+
 // Test Route
 app.get("/", (req, res) => {
   res.send("AssetFlow Backend Running");

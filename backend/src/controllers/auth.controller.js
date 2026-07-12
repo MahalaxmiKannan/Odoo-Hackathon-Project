ae5a274317db011eba198629a2a035d4d2f845bd
Shruthi C <727724euai149@skcet.ac.in>
2026-07-12 11:55:36 +0530

    Completed auth routes, validators and middleware

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const authService = require("../services/auth.service");
+
+exports.signup = async (req, res) => {
+
+};
+
+exports.login = async (req, res) => {
+
+};
+
+exports.forgotPassword = async (req, res) => {
+
+};
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,41 @@
+ const jwt = require("jsonwebtoken");
+
+exports.authenticate = (req, res, next) => {
+
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+
+        return res.status(401).json({
+
+            success: false,
+
+            message: "Unauthorized"
+
+        });
+
+    }
+
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
+
+        next();
+
+    }
+
+    catch {
+
+        return res.status(401).json({
+
+            success: false,
+
+            message: "Invalid Token"
+
+        });
+
+    }
+
+};
